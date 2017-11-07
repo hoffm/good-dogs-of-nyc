532bfc2ca55b66c00a73d5b68ed34904e9ca13a7
Michael Hoffman <michael.s.hoffman@gmail.com>
2017-11-07 15:43:42 -0500

    Create and use test DB in test env

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,12 @@ require "active_record"
 require "pg"
 require "logger"
 
+ENV["RACK_ENV"] ||= "development"
+
 ActiveRecord::Base.logger = Logger.new("debug.log")
 configuration = YAML::safe_load(IO.read("db/config.yml"))
-ActiveRecord::Base.establish_connection(configuration["development"])
+p
+ActiveRecord::Base.establish_connection(configuration[ENV["RACK_ENV"]])
 
 %w(models services).each do |dir|
   Dir[Dir.pwd + "/lib/#{dir}/**/*.rb"].each { |f| require f }
